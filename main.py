16afb65d08d02e85fe0a0f87970b7a243f939cbf
gunjanak <janak.klal@gmail.com>
2025-01-13 23:37:34 +0545

    plots edited

--- main.py
+++ main.py
@@ -164,17 +164,17 @@ def for_pytorch():
     st.pyplot(fig)
     
     # Create the figure
-    fig, ax = plt.subplots()  # 3 rows, 5 columns
-    # axes = axes.flatten()
+    fig, axes = plt.subplots()  # 3 rows, 5 columns
+    axes = axes.flatten()
     print(interval_prediction[3])
-    ax.scatter(X[:, 0], X[:, 1], c=interval_prediction[3], cmap='viridis')
+    # ax.scatter(X[:, 0], X[:, 1], c=interval_prediction[3], cmap='viridis')
     ax.set_title(f"After {epoch} epochs", fontsize=10)
-    # for i, ax in enumerate(axes):
-    #     epoch = i * 10
-    #     ax.scatter(X[:, 0], X[:, 1], c=interval_prediction[i], cmap='viridis')
-    #     ax.set_title(f"After {epoch} epochs", fontsize=10)
-    #     ax.set_xticks([])  # Optional: Remove x-axis ticks for cleaner look
-    #     ax.set_yticks([])  # Optional: Remove y-axis ticks for cleaner look
+    for i, ax in enumerate(axes):
+        epoch = i * 10
+        ax.scatter(X[:, 0], X[:, 1], c=interval_prediction[i], cmap='viridis')
+        ax.set_title(f"After {epoch} epochs", fontsize=10)
+        ax.set_xticks([])  # Optional: Remove x-axis ticks for cleaner look
+        ax.set_yticks([])  # Optional: Remove y-axis ticks for cleaner look
 
     # Adjust layout
     plt.tight_layout()
