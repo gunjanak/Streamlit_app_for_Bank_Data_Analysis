314a7a79e98310088b196ba8cad20408e2479c5c
gunjanak <janak.klal@gmail.com>
2025-01-13 23:12:19 +0545

    plots edited

--- main.py
+++ main.py
@@ -166,12 +166,14 @@ def for_pytorch():
     # Create the figure
     fig, axes = plt.subplots(3, 5, figsize=(16, 8))  # 3 rows, 5 columns
     axes = axes.flatten()
-    for i, ax in enumerate(axes):
-        epoch = i * 10
-        ax.scatter(X[:, 0], X[:, 1], c=interval_prediction[i], cmap='viridis')
-        ax.set_title(f"After {epoch} epochs", fontsize=10)
-        ax.set_xticks([])  # Optional: Remove x-axis ticks for cleaner look
-        ax.set_yticks([])  # Optional: Remove y-axis ticks for cleaner look
+    ax.scatter(X[:, 0], X[:, 1], c=interval_prediction[3], cmap='viridis')
+    ax.set_title(f"After {epoch} epochs", fontsize=10)
+    # for i, ax in enumerate(axes):
+    #     epoch = i * 10
+    #     ax.scatter(X[:, 0], X[:, 1], c=interval_prediction[i], cmap='viridis')
+    #     ax.set_title(f"After {epoch} epochs", fontsize=10)
+    #     ax.set_xticks([])  # Optional: Remove x-axis ticks for cleaner look
+    #     ax.set_yticks([])  # Optional: Remove y-axis ticks for cleaner look
 
     # Adjust layout
     plt.tight_layout()
